059225578adeb54000f20eea10ac9c77486b523b
Kevan Ahlquist <ahlqu039@umn.edu>
2016-05-16 23:23:07 -0500

    Create food reports when API is hit.

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
+var models  = require('../models');
+
 /* GET home page. */
 router.get('/', (req, res, next) => {
   res.render('index', { title: 'Food Finder' });
 });
 
 router.post('/reportFood', function (req, res) {
-  res.json({
-    success: true,
-    request: {
+  models.FoodReport
+    .create({
       latitude: req.body.latitude,
-      longitude: req.body.longitude,
-      accuracy: req.body.accuracy
-    }});
+      longitude: req.body.longitude
+    })
+    .then(({ dataValues }) => {
+      res.json({
+        success: true,
+        id: dataValues.id,
+        request: {
+          latitude: req.body.latitude,
+          longitude: req.body.longitude,
+          accuracy: req.body.accuracy
+        }});
+    });
 });
 
 module.exports = router;
